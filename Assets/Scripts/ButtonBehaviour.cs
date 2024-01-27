5c3bfd6f37565be2803a6f799e51a9fc04820b16
ChiaraLoca <chiara.locatelli.395@gmail.com>
2024-01-27 16:08:56 +0100

    Word

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -10,6 +11,8 @@ public class ButtonBehaviour : MonoBehaviour
     [SerializeField] private string _keyWord;
     [SerializeField] private int _indexOfCommand;
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private GameObject _hintGo;
+    private int baitClickCounter = 0;
     UnityAction _realAction;
     public void Create(UnityAction realAction)
     {
@@ -21,10 +24,18 @@ public class ButtonBehaviour : MonoBehaviour
 
     public void BaitClick()
     {
+
         string res = KeywordRecognizerManager.Instance.GetRandomKeyWord(_indexOfCommand);
         if (!res.Equals(""))
             _keyWord = res;
         _text.text = _keyWord.ToUpper();
+        baitClickCounter++;
+        if (baitClickCounter == 3)
+        {
+            _hintGo.SetActive(true);
+            baitClickCounter = 0;
+            StartCoroutine(WaitAndDisableHint());
+        }
     }
 
     public void PhraseRecognized(string phrase,int index)
@@ -33,4 +44,10 @@ public class ButtonBehaviour : MonoBehaviour
         if (phrase.ToLower().Equals(_keyWord.ToLower()) && index== _indexOfCommand)
             _realAction.Invoke();
     }
+
+    public IEnumerator WaitAndDisableHint()
+    {
+        yield return new WaitForSeconds(3);
+        _hintGo.SetActive(false);
+    }    
 }
