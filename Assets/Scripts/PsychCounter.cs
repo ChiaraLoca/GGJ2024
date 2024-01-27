d61d5ae3a67694c6bacc900a5aba4a4822d801e6
jobbo994 <jobbo994@gmail.com>
2024-01-27 14:33:24 +0100

    fix

---
+++ Assets/Scripts/PsychCounter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PsychCounter : MonoBehaviour
+{
+
+    public static PsychCounter Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    [SerializeField] GameObject prefab;
+    [SerializeField] float OffsetX;
+    [SerializeField] float OffsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void AddMushrooms()
+    {
+        GameObject newInstance = Instantiate(prefab, transform.GetChild(0));
+        //fACCIO COSE
+    }
+}
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -22,6 +22,8 @@ public class playerScript : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         collider2D = GetComponent<Collider2D>();
         rigidbody2D.velocity = new Vector2(horizontalAcceleration, 0f);
+       
+
     }
 
     // Update is called once per frame
@@ -87,7 +89,10 @@ public class playerScript : MonoBehaviour
     private void collectMushroom(Collider2D collision)
     {
         gameStatus.Mushrooms++;
+        PsychCounter.Instance.AddMushrooms();
         modifySpeed(collision.gameObject.GetComponent<mushroom>().StackValue);
         collision.gameObject.SetActive(false);
     }
+
+ 
 }
